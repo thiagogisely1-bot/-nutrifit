d75be647970345af0a93f7f46b926ba51ef24df8
Thigas1007 <thiagogisely1@gmail.com>
2026-07-07 21:06:20 -0300

    Add files via upload

---
+++ nutricao/server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+  const ext = path.extname(filePath);
+  const types = {
+    '.html': 'text/html; charset=utf-8',
+    '.css': 'text/css; charset=utf-8',
+    '.js': 'application/javascript; charset=utf-8',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+  };
+
+  try {
+    const content = fs.readFileSync(filePath);
+    res.writeHead(200, { 'Content-Type': types[ext] || 'text/plain' });
+    res.end(content);
+  } catch {
+    res.writeHead(404);
+    res.end('404');
+  }
+});
+
+server.listen(3001, () => {
+  console.log('Servidor: http://localhost:3001');
+  console.log('Aperte Ctrl+C para parar');
+});
